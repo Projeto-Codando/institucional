6ce84952bbba79a6ddf6d4fcbb9da5413a7c07c6
trajanomarcos <marcosvtrajano@outlook.com>
2024-06-08 03:40:22 -0300

    Comittando alterações no arquivo portalSala.jsx

--- src/screens/portalSala/portalSala.jsx
+++ src/screens/portalSala/portalSala.jsx
@@ -8,32 +8,24 @@ import { useEffect, useState } from 'react';
 import Estudantes from '../../componentes/estudantes/estudantes';
 import api from '../../api';
 import { toast } from 'react-toastify';
-import React, { useEffect, useState } from 'react';
 
-function PortalSala() {
-
-    const [isAlunoLoggedIn, setIsAlunoLoggedIn] = useState(false);
-    const [isProfessorLoggedIn, setIsProfessorLoggedIn] = useState(false);
-
-    useEffect(() => {
-
-        const apelido = sessionStorage.getItem("apelido");
-        const email = sessionStorage.getItem("email");
-
-        if (apelido) {
-            setIsAlunoLoggedIn(true);
-        }
-        if (email) {
-            setIsProfessorLoggedIn(true);
-        }
-
-    }, []);
-
-<<<<<<< HEAD
 function PortalSala() {
   const [turmaData, setTurmaData] = useState(null);
+  const [isAlunoLoggedIn, setIsAlunoLoggedIn] = useState(false);
+  const [isProfessorLoggedIn, setIsProfessorLoggedIn] = useState(false);
 
   useEffect(() => {
+
+    const apelido = sessionStorage.getItem("apelido");
+    const email = sessionStorage.getItem("email");
+
+    if (apelido) {
+      setIsAlunoLoggedIn(true);
+    }
+    if (email) {
+      setIsProfessorLoggedIn(true);
+    }
+
     const turmaBuscada = sessionStorage.getItem('idTurmaClicada');
 
     api.get(`/turmas/buscar-turma-por-id/${turmaBuscada}`, {
@@ -41,7 +33,7 @@ function PortalSala() {
         Authorization: `Bearer ${sessionStorage.getItem("token")}`
       }
     }).then((json) => {
-      if(json.data === null){
+      if (json.data === null) {
         toast.error("Não foi possível encontrar a turma!");
       } else {
         setTurmaData(json.data);
@@ -52,12 +44,21 @@ function PortalSala() {
   }, []);
 
   if (!turmaData) {
-    return null; // or return a loading spinner
+    return null; 
   }
 
   return (
     <div className='body'>
-      <Header className='container'/>
+      <Header
+        logo={Logo}
+        statusLogoff={isAlunoLoggedIn || isProfessorLoggedIn ? null : "true"}
+        statusLogin={isAlunoLoggedIn || isProfessorLoggedIn ? "true" : null}
+        statusLoginAluno={isAlunoLoggedIn ? "true" : null}
+        statusLoginProfessor={isProfessorLoggedIn ? "true" : null}
+        statusEstrela={isAlunoLoggedIn ? "true" : null}
+        statusSerie={isAlunoLoggedIn ? "true" : null}
+        statusAvatar={isAlunoLoggedIn || isProfessorLoggedIn ? "true" : null}
+      />
       <div className='portalSala'>
         <FaixaTurma
           text={turmaData.nome}
@@ -66,34 +67,6 @@ function PortalSala() {
         />
         <div className='telaPrincipal'>
           <Progresso />
-=======
-    return (
-        <div >
-            <Header
-                logo={Logo}
-                statusLogoff={isAlunoLoggedIn || isProfessorLoggedIn ? null : "true"}
-                statusLogin={isAlunoLoggedIn || isProfessorLoggedIn ? "true" : null}
-                statusLoginAluno={isAlunoLoggedIn ? "true" : null}
-                statusLoginProfessor={isProfessorLoggedIn ? "true" : null}
-                statusEstrela={isAlunoLoggedIn ? "true" : null}
-                statusSerie={isAlunoLoggedIn ? "true" : null}
-                statusAvatar={isAlunoLoggedIn || isProfessorLoggedIn ? "true" : null}
-            />
-            <div className='portalSala'>
-                <FaixaTurma
-                    text="6C-2024"
-                    escolaridade="6º ano"
-                    codigo="31204"
-                />
-                <div className='telaPrincipal'>
-                <Progresso
-                
-                />
-                </div>
-             
-                
-            </div>
->>>>>>> 03001fffd3cf084827424451f6f6d2565e2b35a3
         </div>
       </div>
     </div>
