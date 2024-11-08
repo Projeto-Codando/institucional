9303359c9dec4251503d3bbbe39015273bc1461c
ismaelOG1 <matheus.lourenco@sptech.school>
2024-11-08 00:52:32 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'http://54.210.10.240/api', // Troque pelo IP público da sua instância
 });
 
 export default api;
