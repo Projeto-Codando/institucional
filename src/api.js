d2032628a5bb4b1b7c05d16f390080e756ee09dd
matheus ismael <125322046+ismaelOG1@users.noreply.github.com>
2024-11-20 12:24:30 -0300

    Alteraçõoes api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://54.210.10.240/api', // Troque pelo IP público da sua instância
+  baseURL: 'https://54.210.10.240/api', // Troque pelo IP público da sua instância
 });
 
 export default api;
