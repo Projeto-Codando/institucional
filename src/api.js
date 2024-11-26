7a3065803298870b2ab1ff33ee1bbf163c631602
matheus ismael <125322046+ismaelOG1@users.noreply.github.com>
2024-11-25 21:26:48 -0300

    .

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://54.210.10.240/api', // Troque pelo IP público da sua instância
+  baseURL: 'https://codando.hopto.org/api', // Troque pelo IP público da sua instância
 });
 
 export default api;
