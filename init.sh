f28eadca0aa59e1e4ef0c1cb2a50c6851e528442
matheus ismael <125322046+ismaelOG1@users.noreply.github.com>
2024-11-28 21:22:13 -0300

    Alterações para apontar para novo domínio + certbot correto

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Caminhos dos certificados
-SSL_DIR="/etc/letsencrypt/live/codando.hopto.org"
+SSL_DIR="/etc/letsencrypt/live/codandoapp.me"
 SSL_CERT="${SSL_DIR}/fullchain.pem"
 SSL_KEY="${SSL_DIR}/privkey.pem"
 
 # Realiza a obtenção do certificado SSL com o Certbot
 echo "Obtendo certificado SSL com Certbot..."
-certbot certonly --standalone --agree-tos --email seu-email@dominio.com -d codando.hopto.org
+certbot certonly --standalone --agree-tos --email matheus.lourenco@sptech.school -d codandoapp.me -d www.codandoapp.me
 
 # Verifica se o certificado foi obtido com sucesso
 if [ ! -f "$SSL_CERT" ]; then
