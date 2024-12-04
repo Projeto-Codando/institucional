e6d1987f30d7e84e12c6ee3b9697430616cd0815
matheus ismael <125322046+ismaelOG1@users.noreply.github.com>
2024-12-04 12:18:13 -0300

    Removendo hopt.org por conta de limite excedido

--- init.sh
+++ init.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-# Caminho padrão para os certificados Let's Encrypt
+
 SSL_CERT="/etc/letsencrypt/live/codando.hopto.org/fullchain.pem"
 SSL_KEY="/etc/letsencrypt/live/codando.hopto.org/privkey.pem"
 
-# Obtém certificados SSL válidos com Certbot, se não existirem
 if [ ! -f "$SSL_CERT" ] || [ ! -f "$SSL_KEY" ]; then
     echo "Obtendo certificados SSL com Certbot..."
     certbot certonly --standalone --agree-tos --email matheus.lourenco@sptech.school \
-        -d codando.hopto.org -d codandoapp.me -d www.codandoapp.me
+        # -d codando.hopto.org 
+        -d codandoapp.me -d www.codandoapp.me
 else
     echo "Certificados SSL já existem. Pulando geração."
 fi
 
-# Inicia o Nginx
 echo "Iniciando o Nginx..."
 nginx -g "daemon off;"
