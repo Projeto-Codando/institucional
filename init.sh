a71c8af02e99581a122d8480029a7f6edc5cc569
matheus ismael <125322046+ismaelOG1@users.noreply.github.com>
2024-12-10 14:25:04 -0300

    Trocando BuySSL para ZeroSSL

--- init.sh
+++ init.sh
@@ -6,16 +6,12 @@ SSL_KEY="/etc/letsencrypt/live/codandoapp.me/privkey.pem"
 if [ ! -f "$SSL_CERT" ] || [ ! -f "$SSL_KEY" ]; then
     echo "Obtendo certificados SSL com Certbot..."
     certbot certonly --standalone \
-  --server https://acme.zerossl.com/v2/DV90 \
-  -d codandoapp.me -d www.codandoapp.me \
-  --agree-tos --email matheus.lourenco@sptech.school
-
-fi
-
-if [ -f "$SSL_CERT" ] && [ -f "$SSL_KEY" ]; then
-    echo "Certificados SSL encontrados. Iniciando o Nginx..."
-    nginx -g "daemon off;"
+      -d codandoapp.me -d www.codandoapp.me \
+      --agree-tos --email matheus.lourenco@sptech.school \
+      --non-interactive
 else
-    echo "Erro: Certificados SSL não encontrados. Abortando..."
-    exit 1
+    echo "Certificados SSL já existem. Pulando geração."
 fi
+
+echo "Iniciando o Nginx..."
+nginx -g "daemon off;"
