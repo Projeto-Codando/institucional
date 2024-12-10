39fefa7d922b9e0a43138c906f002cb38801b2be
matheus ismael <125322046+ismaelOG1@users.noreply.github.com>
2024-12-10 14:19:08 -0300

    Trocando BuySSL para ZeroSSL

--- init.sh
+++ init.sh
@@ -6,9 +6,10 @@ SSL_KEY="/etc/letsencrypt/live/codandoapp.me/privkey.pem"
 if [ ! -f "$SSL_CERT" ] || [ ! -f "$SSL_KEY" ]; then
     echo "Obtendo certificados SSL com Certbot..."
     certbot certonly --standalone \
-        --server https://api.buypass.com/acme/directory \
-        -d codandoapp.me -d www.codandoapp.me \
-        --agree-tos --email matheus.lourenco@sptech.school
+  --server https://acme.zerossl.com/v2/DV90 \
+  -d codandoapp.me -d www.codandoapp.me \
+  --agree-tos --email matheus.lourenco@sptech.school
+
 fi
 
 if [ -f "$SSL_CERT" ] && [ -f "$SSL_KEY" ]; then
